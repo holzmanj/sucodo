c1efa7a4f5e48aede4e245a3e3392842b92ad2e3
Jesse <jesseholzman96@gmail.com>
2021-12-12 20:48:45 -0500

    Take puzzle file as command-line arg

--- app/Main.hs
+++ app/Main.hs
@@ -3,10 +3,15 @@ module Main where
 import Control.Comonad
 import Grid
 import Puzzle
+import System.Environment
 
 main :: IO ()
 main = do
-  txt <- readFile "sample.txt"
-  let ns   = read <$> (concat $ words <$> lines txt)
-  let puzz = fromIntList ns
-  print $ until (not . any wasUpdated) (extend stepTile) puzz
+  as <- getArgs
+  if null as
+    then putStrLn "Need path to sudoku puzzle"
+    else do
+      txt <- readFile (head as)
+      let ns   = read <$> concat (words <$> lines txt)
+      let puzz = fromIntList ns
+      print $ until (not . any wasUpdated) (extend stepTile) puzz
